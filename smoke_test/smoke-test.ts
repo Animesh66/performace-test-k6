d74c66521192d4ded4638d10fb381a6c5bf9dc51
Animesh66 <er.animesh6@hotmail.com>
2025-08-23 23:23:11 +1000

    Fix the smoke test.

--- smoke_test/smoke-test.ts
+++ smoke_test/smoke-test.ts
@@ -8,13 +8,14 @@ export let options = {
 
 export default function () {
   let res = http.get('https://quickpizza.grafana.com/test.k6.io/');
-  check(res, { 'Status is 200': (r) => r.status === 200 });
+  check(res, { 'Status is 200': (r) => r.status === 200,
+    'Page is correct': (r) => r.body?.toString().includes('QuickPizza Legacy') ?? false});
   sleep(1);
   res = http.get('https://quickpizza.grafana.com/contacts.php');
-  check(res, { 'Status is 200': (r) => r.status === 200 });
+  check(res, { 'Status is 200': (r) => r.status === 200});
   sleep(1)
   res = http.get('https://quickpizza.grafana.com/pi.php?decimals=3');
-  check(res, { 'Status is 200': (r) => r.status === 200 });
+  check(res, { 'Status is 200': (r) => r.status === 200});
   sleep(1)
   res = http.get('https://quickpizza.grafana.com/flip_coin.php');
   check(res, { 'Status is 200': (r) => r.status === 200 });
